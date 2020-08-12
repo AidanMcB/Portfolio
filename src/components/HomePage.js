8bd53b957891c8614ca99aae77b4ba063ffcf3df
AidanMcB <aidankmcbride@gmail.com>
2020-08-12 15:41:16 -0400

    home page about me added and profile pic styled with footer

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 // Images
 import BGimg from '../assets/codeComp.jpg'
+import ProfPic from '../assets/profile-pic.jpeg'
 // Components 
 import NavBar from './NavBar'
 // Styles 
@@ -24,11 +25,27 @@ export default function HomePage() {
 
     return (
         <div className="home-page">
-                <div className="bg-img-container" >
-                    <img className="bg-img" src={BGimg} />
-                </div>
-            <h1 className="hi-header">Hi I'm Aidan, a web devloper</h1>
-            {/* about me */}
+            <div className="bg-img-container" >
+                <img className="bg-img" src={BGimg} alt="computer header image"/>
+            </div>
+            <h1 className="hi-header">Aidan McBride</h1>
+            <h3 className="hi-header">Web Developer</h3>
+            <div className="align-pic-about">
+            <div className="profile-pic-container">
+                <img className="profile-pic" src={ProfPic} alt="profile picture" />
+            </div>
+            <div className="about-me">
+                <h5 className="about-me-header">About Me</h5>
+                <p>I have a passion for front end web development. Since I began programming, it always amazed me to see what could be rendered from my imagination to a computer screen. I got the opportunity to hone my craft at Flatiron School in Houston Texas in March of 2020. There I gained experience in full stack developement with Ruby on Rails, JavaScript, SQL and much more. In my spare time when I can usually be found building projects and learning more about programming. Or hanging out with my cat. I am currently looking for new opportunities in the field of web development, meeting new people, and expanding my horizons.</p>
+            </div>
+            </div>
+            <div className="footer">
+                <ul className="footer-list">
+                    <li><a href="https://github.com/AidanMcB">GitHub</a></li>
+                    <li><a href="https://www.youtube.com/channel/UCO5m40AxTx4AuitZQGx3Q4w?">YouTube</a></li>
+                    <li><a href="https://www.linkedin.com/in/aidan-mcbride-52b6261a9/">Linkedin</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
