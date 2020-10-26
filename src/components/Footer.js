bd717283f084551d71ed372f87938c6f4e86cfc5
Aidan McBride <aidankmcbride@gmail.com>
2020-10-25 21:39:01 -0400

    footer wip

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ import myResume from '../assets/myResume.pdf'
 // Styles
 import {
     Container,
-    Icon
+    Icon,
+    Menu,
+    Segment
 } from 'semantic-ui-react'
 
 export default function Footer() {
@@ -12,13 +14,13 @@ export default function Footer() {
     const history = useHistory()
 
     return (
-        <div className="footer">
-            <Container className="footer-links">
-                <Icon onClick={() => window.location = 'https://github.com/AidanMcB'} className="github-icon" name="github" size="huge" />
-                <Icon onClick={() => window.location = "https://www.youtube.com/channel/UCO5m40AxTx4AuitZQGx3Q4w/videos?"} className="youtube-icon" name="youtube" size="huge" color="red" />
-                <Icon onClick={() => window.location = "https://medium.com/@aidankmcbride"} className="medium-icon" name="medium" size="huge" />
-                <div className="resume-link-div"><a className="resume-link" href={myResume} download>Download my resume</a></div>
-            </Container>
-        </div>
+        <Segment inverted className="footer">
+            <Menu inverted secondary className="footer-links" >
+                <Menu.Item as='a' target="_blank" href="https://github.com/AidanMcB"><Icon className="github-icon" name="github" size="huge" /></Menu.Item>
+                <Menu.Item as='a' target="_blank" href="https://www.youtube.com/channel/UCO5m40AxTx4AuitZQGx3Q4w/videos?"><Icon className="youtube-icon" name="youtube" size="huge" color="red" /></Menu.Item>
+                <Menu.Item as='a' target="_blank" href="https://medium.com/@aidankmcbride"><Icon className="medium-icon" name="medium" size="huge" /></Menu.Item>
+                <Menu.Item as='a' target="_blank" href={myResume} download><Icon className="resume-icon" name="download" size="huge" /></Menu.Item>
+            </Menu>
+        </Segment>
     )
 }
